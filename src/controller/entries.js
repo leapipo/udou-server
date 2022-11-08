53c9478131bed711b26497c0521426d3fef247bd
L-Pipo <94913915+L-Pipo@users.noreply.github.com>
2022-11-08 12:08:43 +0100

    Cleaned up code

--- src/controller/entries.js
+++ src/controller/entries.js
@@ -3,18 +3,6 @@ const express = require('express');
 const router = express.Router();
 const entriesService = require('../service/entries');
 
-// router.get('/', (req, res) => { --> gleich
-// entriesService.getEntries --> anders
-// service folder entries
-// entriesService ist ein Objekt mit Methoden
-// u.a. auch getEntries
-// getEntries returnes entriesModel.getEntries
-// model folder entries
-// entriesModel ist ein Objekt mit Methoden
-// u.a. auch getEntries
-// hier wird Verbindung zur DB aufgebaut
-// getEntries gibt bspw. alle Einträge aus entries table zurück
-
 router.get('/', (req, res) => {
   entriesService
     .getEntries()
